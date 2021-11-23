40d9d25c5888dc0ff24318592cd4a2709ab9eadf
Jose Navarrete <jose@MacBook-Pro.local>
2021-11-23 10:58:10 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ejemplo1';
+  show:boolean = true;
+  show2:boolean = false;
+  fondo = '';
+  activo = 'primero';
+
+  cursos:string[] = ['CSS', 'ANGULAR', 'COBOL', 'PHP', 'PASCAL','HTML', 'JAVASCRIPT'];
+  animales:Array<any> = [
+    {tipo:'Perro', nombre:'chuy', edad:2},
+    {tipo:'Gato', nombre:'toby', edad:1},
+    {tipo:'Perro', nombre:'max', edad:3},
+    {tipo:'Gato', nombre:'theo', edad:1},
+    {tipo:'Gato', nombre:'jugo', edad:4},
+    {tipo:'Perro', nombre:'tango', edad:7},
+  ];
+  contador_pos:number=0;
+  contador_neg:number=0;
 }
